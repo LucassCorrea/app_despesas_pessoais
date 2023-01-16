f57de7c839f4e0902026c538831f873f3a6963de
Lucas Correa <lucas.silva.correa07@aluno.ifce.edu.br>
2023-01-16 10:30:15 -0300

    add novas alterações

--- lib/views/components/chart.dart
+++ lib/views/components/chart.dart
@@ -30,10 +30,10 @@ class Chart extends StatelessWidget {
       }
 
       return {
-        'day': DateFormat.E().format(weekDay)[0],
+        'day': DateFormat.E("pt_BR").format(weekDay)[0].toUpperCase(),
         'value': totalSum,
       };
-    });
+    }).reversed.toList();
   }
 
   double get _weekTotalValue {
